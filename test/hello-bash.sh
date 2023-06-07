e78468d5a2169f9bfc8bc3fa5c632b091cc10717
capxh73 <130162611+capxh73@users.noreply.github.com>
2023-06-08 06:54:18 +0800

    hello-bash.sh

--- test/hello-bash.sh
+++ test/hello-bash.sh
@@ -13,10 +13,10 @@ echo "RUN npm install --registry=https://registry.npm.taobao.org" >> Dockerfile
 echo "EXPOSE 3000" >> Dockerfile
 
 # 镜像名和仓库名和标签号
-IMAGE_NAME="test"
+# IMAGE_NAME="test"
 
 # 构建 image 文件
-docker image build -t $IMAGE_NAME . 
+docker image build -t test . 
 
 # 生成容器
 docker container run --rm -p 8000:3000 -it $IMAGE_NAME /bin/bash
@@ -28,9 +28,10 @@ echo "success!"
 docker login --username=$DOCKERHUB_USERNAME --password=$DOCKERHUB_PASSWORD
 
 #为本地的 image 标注用户名和版本。
-docker image tag $IMAGE_NAME capxh73/$IMAGE_NAME
+docker image tag test capxh73/test
+
 #发布 image 文件
-docker push capxh73/$IMAGE_NAME
+docker push capxh73/test
 
 # 执行下面的命令
 #node test/hello-world.js
